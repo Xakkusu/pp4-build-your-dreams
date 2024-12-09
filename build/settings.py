6db3b5d43c56ab793ccf980d029d52ef87ef8c78
Naomi Dax <imkadax@gmail.com>
2024-12-09 10:38:50 +0000

    maint: Create secret key

--- build/settings.py
+++ build/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c@-%6ed@03tovx8-qdo@xz#q72@20yo(4a^!)p-amvtxsms4o&'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-xakkusu-pp4builtyourdre-lcljug1fkwb.ws.codeinstitute-ide.net','.herokuapp.com']
 
